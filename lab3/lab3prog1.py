29e31d1781ce29cc84a2232abb15fe6061fa5d5e
Strike9 <c13741321@mydit.ie>
2015-09-28 15:56:52 +0000

    1st commit

---
+++ lab3/lab3prog1.py
@@ -0,0 +1,15 @@
+
+
+vars = []
+vars[1] = "Oxo"
+vars[2] = "OXO"
+vars[3] = "123454321"
+vars[4] = "ROTATOR"
+vars[5] = "12345 54321"
+
+for i in range (len(vars)):
+  if var[1] == var[1] [::1]:
+    print "true"
+  else:
+
+    print "false"
